7fa4c58fe7211d56e9b61476578fe4896ba817a0
redlimerock <kenchung.poon@gmail.com>
2015-10-27 18:40:51 +0800

    changed many
    changed many

---
+++ FYP16_WinFormsApp_II/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace FYP16_WinFormsApp_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cmbEdgeDetection = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1232, 627);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tableLayoutPanel2.ColumnCount = 10;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cmbEdgeDetection, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox1, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox2, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.trackBar1, 6, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 7, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox2, 8, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox3, 9, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1226, 37);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(125, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Vector";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(247, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 31);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Save SVG";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cmbEdgeDetection
+            // 
+            this.cmbEdgeDetection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbEdgeDetection.FormattingEnabled = true;
+            this.cmbEdgeDetection.Items.AddRange(new object[] {
+            "Img Convolution Filter"});
+            this.cmbEdgeDetection.Location = new System.Drawing.Point(369, 3);
+            this.cmbEdgeDetection.Name = "cmbEdgeDetection";
+            this.cmbEdgeDetection.Size = new System.Drawing.Size(116, 21);
+            this.cmbEdgeDetection.TabIndex = 3;
+            this.cmbEdgeDetection.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(491, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Trace On";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(613, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Points On";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.Location = new System.Drawing.Point(735, 3);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(116, 31);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 130;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(857, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(979, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1101, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1226, 578);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "svg";
+            this.saveFileDialog1.FileName = "*.svg";
+            this.saveFileDialog1.Filter = "svg files (*.svg)|*.svg";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "bmp";
+            this.openFileDialog1.FileName = "*.jpg";
+            this.openFileDialog1.Filter = " Bmp files (*.bmp)|*.bmp|Jpg files (*.jpg) |*.jpg,*.jpeg|Png files (*.png)|*.png|" +
+    "Gif files (*.gif)|*.gif|Tif files (*.tif)|*.tif,*.tiff|All (*.*)|*.*";
+            this.openFileDialog1.FilterIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 627);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmbEdgeDetection;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+    }
+}
+
---
+++ FYP16_WinFormsApp_II/Form1.cs
@@ -0,0 +1,328 @@
+﻿//Copyright (C) 2001-2007 Peter Selinger
+
+//Copyright (C) 2009 Wolfgang Nagl
+
+//This program is free software; you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by  the Free Software Foundation; either version 2 of the License, or (at  your option) any later version.
+
+//This program is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  General Public License for more details.
+
+//You should have received a copy of the GNU General Public License  along with this program; if not, write to the Free Software  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,  USA. See also http://www.gnu.org/.
+
+//See the file COPYING for details.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using CsPotrace;
+
+using System.Linq;
+using System.IO;
+
+
+namespace FYP16_WinFormsApp_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+
+        {
+            InitializeComponent();
+
+            cmbEdgeDetection.SelectedIndex = 0; /*edge detection*/
+        }
+        /// <summary>
+        /// img edge detection
+        /// </summary>
+        private Bitmap originalBitmap = null;
+        private Bitmap previewBitmap = null;
+        private Bitmap resultBitmap = null;
+
+        /// <summary>
+        /// potrace draw
+        /// </summary>
+        private void draw()
+        {
+            if (ListOfCurveArray == null) return;
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            GraphicsPath gp = new GraphicsPath();
+            for (int i = 0; i < ListOfCurveArray.Count; i++)
+            {
+                ArrayList CurveArray = (ArrayList)ListOfCurveArray[i];
+                GraphicsPath Contour = null;
+                GraphicsPath Hole = null;
+                GraphicsPath Current = null;
+
+                for (int j = 0; j < CurveArray.Count; j++)
+                {
+
+                    if (j == 0)
+                    {
+                        Contour = new GraphicsPath();
+                        Current = Contour;
+                    }
+                    else
+                    {
+
+                        Hole = new GraphicsPath();
+                        Current = Hole;
+
+                    }
+                    Potrace.Curve[] Curves = (Potrace.Curve[])CurveArray[j];
+                    float factor = 1;
+                    if (checkBox2.Checked)
+                        //factor = (trackBar1.Value + 1);
+                    for (int k = 0; k < Curves.Length; k++)
+                    {
+                        if (Curves[k].Kind == Potrace.CurveKind.Bezier)
+                            Current.AddBezier((float)Curves[k].A.x * factor, (float)Curves[k].A.y * factor, (float)Curves[k].ControlPointA.x * factor, (float)Curves[k].ControlPointA.y * factor,
+                                        (float)Curves[k].ControlPointB.x * factor, (float)Curves[k].ControlPointB.y * factor, (float)Curves[k].B.x * factor, (float)Curves[k].B.y * factor);
+                        else
+                            Current.AddLine((float)Curves[k].A.x * factor, (float)Curves[k].A.y * factor, (float)Curves[k].B.x * factor, (float)Curves[k].B.y * factor);
+
+                    }
+                    if (j > 0) Contour.AddPath(Hole, false);
+                }
+                gp.AddPath(Contour, false);
+            }
+            if (checkBox1.Checked)
+                g.DrawPath(Pens.Red, gp);
+
+        }
+        private void showPoints()
+        {
+            if (ListOfCurveArray == null) return;
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            for (int i = 0; i < ListOfCurveArray.Count; i++)
+            {
+                ArrayList CurveArray = (ArrayList)ListOfCurveArray[i];
+                for (int j = 0; j < CurveArray.Count; j++)
+                {
+                    Potrace.Curve[] Curves = (Potrace.Curve[])CurveArray[j];
+
+                    float factor = 1;
+                    if (checkBox2.Checked)
+                        factor = (trackBar1.Value + 1);
+                    for (int k = 0; k < Curves.Length; k++)
+                    {
+                        g.FillRectangle(Brushes.Yellow, (float)((Curves[k].A.x) * factor - 1.5), (float)((Curves[k].A.y) * factor - 1.5), 3, 3);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Load img edge detect
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                originalBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+                previewBitmap = originalBitmap.CopyToThisCanvas(pictureBox1.Width);
+                pictureBox1.Image = previewBitmap;
+
+                ApplyFilter(true);
+            }
+        }
+        /// <summary>
+        /// refresh picture
+        /// </summary>
+        private void refreshPicture()
+        {
+            if (Matrix == null) return;
+            /*if (radioButton2.Checked)
+            {
+                Bitmap B = Potrace.BinaryToBitmap(Matrix, true);
+                pictureBox1.Width = B.Width * (trackBar1.Value + 1);
+                pictureBox1.Height = B.Height * (trackBar1.Value + 1);
+                pictureBox1.Image = B;
+            }*/
+            else
+            {
+                Bitmap B = new Bitmap(Matrix.GetLength(0) * (trackBar1.Value + 1), Matrix.GetLength(1) * (trackBar1.Value + 1));
+                pictureBox1.Width = B.Width;
+                pictureBox1.Height = B.Height;
+                pictureBox1.Image = B;
+            }
+
+            draw();
+        }
+        private void refreshMatrix()
+        {
+            if (Bitmap == null) return;
+            Matrix = Potrace.BitMapToBinary(Bitmap, trackBar1.Value);
+            refreshPicture();
+
+        }
+
+        bool[,] Matrix;
+        ArrayList ListOfCurveArray;
+        Bitmap Bitmap;
+
+        /// <summary>
+        /// Vectorize 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListOfCurveArray = new ArrayList();
+            Potrace.turdsize = Convert.ToInt32(textBox1.Text);
+            try
+            {
+                Potrace.alphamax = Convert.ToDouble(textBox3.Text);
+            }
+            catch
+            {
+                textBox3.Text = Potrace.alphamax.ToString();
+            }
+            try
+            {
+                Potrace.opttolerance = Convert.ToDouble(textBox2.Text);
+            }
+            catch
+            {
+                textBox2.Text = Potrace.opttolerance.ToString();
+            }
+            //optimize the path p, replacing sequences of Bezier segments by a
+            //single segment when possible.
+            Potrace.curveoptimizing = true; //checkBox4.Checked;
+            Matrix = Potrace.BitMapToBinary(Bitmap, trackBar1.Value);
+            Potrace.potrace_trace(Matrix, ListOfCurveArray);
+            refreshMatrix();
+          
+        }
+        /// <summary>
+        /// Save as SVG
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (ListOfCurveArray == null)
+            {
+                MessageBox.Show("Please vectorize the image first");
+                return;
+            }
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string s = Potrace.Export2SVG(ListOfCurveArray, Bitmap.Width, Bitmap.Height);
+                System.IO.StreamWriter FS = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                FS.Write(s);
+                FS.Close();
+            }
+        }
+        /// <summary>
+        /// Disp pic 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// img edge detection
+        /// </summary>
+        /// <param name="preview"></param>
+        private void ApplyFilter(bool preview)
+         {
+             if (pictureBox1 == null || cmbEdgeDetection.SelectedIndex == -1)
+             {
+                 return;
+             }
+
+             Bitmap selectedSource = null;
+             Bitmap bitmapResult = null;
+
+             if (preview == true)
+             {
+                 selectedSource = previewBitmap;
+             }
+             else
+             {
+                 selectedSource = originalBitmap;
+             }
+
+             if (selectedSource != null)
+             {
+                 if (cmbEdgeDetection.SelectedItem.ToString() == "Img Convolution Filter")
+                 {
+                     bitmapResult = selectedSource;
+                 }
+                 else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 3x3")
+                 {
+                     bitmapResult = selectedSource.Laplacian3x3Filter(false);
+                 }
+               
+             }
+
+             if (bitmapResult != null)
+             {
+                 if (preview == true)
+                 {
+                     pictureBox1.Image = bitmapResult;
+                 }
+                 else
+                 {
+                     resultBitmap = bitmapResult;
+                 }
+             }
+         }
+        /// <summary>
+        /// Dropdown selection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ApplyFilter(true);
+        }
+        /// <summary>
+        /// trackbar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            refreshMatrix();
+            float p = 100 * (float)trackBar1.Value / (float)255;
+            //textBox1.Text = p.ToString("00");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            float p = 100 * (float)trackBar1.Value / (float)255;
+            //textBox1.Text = p.ToString("00");
+            textBox1.Text = Potrace.turdsize.ToString();
+            textBox3.Text = Potrace.alphamax.ToString();
+            textBox2.Text = Potrace.opttolerance.ToString();
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            refreshPicture();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            refreshPicture();
+        }
+    }
+}
